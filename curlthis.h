2f466c220ef4df4a13406757f9680ada4979db42
David Volm <david@volminator.com>
2014-03-25 17:21:26 -0500

    npm oops

--- curlthis.h
+++
@@ -1,6 +0,0 @@
-/* Curlthis
- * grabs predetermined stuff from the internet
- * (c) 2013 David (daXXog) Volm ><> + + + <><
- * Released under Apache License, Version 2.0:
- * http://www.apache.org/licenses/LICENSE-2.0.html  
- */ 
