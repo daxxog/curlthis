b46154e4e863abd8ee0a6ccc732f6bf37b74d426
David Volm <david@volminator.com>
2014-12-22 11:01:07 -0600

    removed old scripts

--- preinstall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Curlthis / preinstall.sh
-# preinstall script for Curlthis
-# (c) 2013 David (daXXog) Volm ><> + + + <><
-# Released under Apache License, Version 2.0:
-# http://www.apache.org/licenses/LICENSE-2.0.html  
-#################################################
-
-touch com-npm-install
